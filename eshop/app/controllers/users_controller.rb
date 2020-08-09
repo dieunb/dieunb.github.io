c0206be658457b67585c28bd5f200af8c6ddf29a
Dieu Nguyen <dieunb@gmail.com>
2020-08-09 18:42:19 +0700

    Sign up user

---
+++ eshop/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = 'You have successfully registered'
+      redirect_to root_path
+    else
+      flash[:warning] = 'Registration failed'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+end
--- eshop/config/routes.rb
+++ eshop/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+
+  get 'sign_up' => 'users#new'
+  post 'sign_up' => 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
