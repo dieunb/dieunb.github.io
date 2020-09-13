73af64e9577f698c4fb78aaa3a337d028a443663
Dieu Nguyen <dieunb@gmail.com>
2020-09-13 09:32:40 +0700

    comments

---
+++ eshop/app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @review = Review.find params[:review_id]
+    @comment = @review.comments.new(comment_params)
+    @comment.user = current_user
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @review.product, notice: 'Comment was successfully created.' }
+        format.js
+        format.json { render json: @comment, status: :created, location: @user }
+      else
+        format.html { render "products/show" }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:review).permit(:content)
+  end
+end
