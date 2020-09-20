60485ec64d02c6d117bb3fd160f79e235d1e85ef
Dieu Nguyen <dieunb@gmail.com>
2020-09-20 21:45:24 +0700

    user role migration and validation

--- eshop/app/models/user.rb
+++ eshop/app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   has_many :reviews, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  validates :role, presence: true, inclusion: { in: ['admin_user', 'normal_user'] }
 end
